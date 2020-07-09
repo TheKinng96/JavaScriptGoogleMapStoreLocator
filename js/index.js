11846a8d08139c3fdcebb938041c1e2a0557de0c
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-07-09 13:01:22 +0900

    connected the database with the map markers

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 let map;
 
 const initMap = () => {
@@ -9,17 +8,46 @@ const initMap = () => {
       zoom: 8,
       styles: nightTheme
   });
-
-  createMarker(mylatlng);
+  getStores();
 }
 
-const createMarker = (mylatlng) => {
+const getStores = () => {
+  const API_URL = 'http://localhost:3000/api/stores';
+  fetch(API_URL)
+  .then((response) => {
+    if (response.status == 200){
+      return response.json()
+    } else {
+      throw new Error(response.status)
+    }
+  }).then((data) => {
+    searchLocationNear(data)
+    // console.log(data)
+  })
+
+}
+
+const searchLocationNear = (stores) => {
+  let bounds = new google.maps.LatLngBounds();
+  stores.forEach((store, index) => {
+    let latlng = new google.maps.LatLng(
+      store.location.coordinates[1],
+      store.location.coordinates[0]
+    )
+    let name = store.storeName;
+    let address = store.addressLines[0];
+    bounds.extend(latlng)
+    createMarker(latlng, name, address)
+  })
+  map.fitBounds(bounds)
+}
+
+const createMarker = (latlng, name, address) => {
   let marker = new google.maps.Marker({
-    position: mylatlng,
+    position: latlng,
     map: map,
     title: 'no idea'
   })
-  marker.setMap(map)
 }
 
 initMap();
