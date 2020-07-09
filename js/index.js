0cadcec32ddf581a1100f7a239b2fb9531fa029f
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-07-09 15:59:18 +0900

    done the element creating for the store list

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 let map;
+let infoWindow;
+let markers=[];
 
-const initMap = () => {
+function initMap ()  {
   let mylatlng = {lat: 34.063380, lng: -118.358080}
 
   map = new google.maps.Map(document.getElementById("map"), {
@@ -8,6 +10,7 @@ const initMap = () => {
       zoom: 8,
       styles: nightTheme
   });
+  infoWindow = new google.maps.InfoWindow();
   getStores();
 }
 
@@ -21,10 +24,47 @@ const getStores = () => {
       throw new Error(response.status)
     }
   }).then((data) => {
-    searchLocationNear(data)
+    searchLocationNear(data);
+    setStoreList(data);
+    setOnClickListener();
     // console.log(data)
   })
+}
 
+const setOnClickListener = () => {
+  let storeElements = document.querySelectorAll('.store-container');
+  storeElements.forEach((elem, index) => {
+    elem.addEventListener('click', () => {
+      google.maps.event.trigger(markers[index], 'click')
+    })
+  })
+}
+
+const setStoreList = (stores) => {
+  let storesHtml = '';
+  stores.forEach((store, index) => {
+    storesHtml += `
+      <div class="store-container">
+          <div class="store-container-background">
+              <div class="store-info-container">
+                  <div class="store-address">
+                      <span>${store.addressLines[0]}</span>
+                      <span>${store.addressLines[1]}</span>
+                  </div>
+                  <div class="store-phone-number">
+                      ${store.phoneNumber}
+                  </div>
+              </div>
+              <div class="store-number-container">
+                  <div class="store-number">
+                      ${index + 1}
+                  </div>
+              </div>
+          </div>
+      </div>
+    `
+  })
+  document.querySelector('.stores-list').innerHTML = storesHtml;
 }
 
 const searchLocationNear = (stores) => {
@@ -36,18 +76,48 @@ const searchLocationNear = (stores) => {
     )
     let name = store.storeName;
     let address = store.addressLines[0];
+    let openStatusText = store.openStatusText;
+    let phone = store.phoneNumber;
     bounds.extend(latlng)
-    createMarker(latlng, name, address)
+    createMarker(latlng, name, address, index, openStatusText, phone)
   })
   map.fitBounds(bounds)
 }
 
-const createMarker = (latlng, name, address) => {
+const createMarker = (latlng, name, address, storeNumber, openStatusText, phone) => {
+  let html = `
+    <div class="store-info-window">
+      <div class="store-info-name">
+        ${name}
+      </div>
+      <div class="store-info-open-status">
+        ${openStatusText}
+      </div>
+      <div class="store-info-address">
+        <div class="icon">
+          <i class="fas fa-location-arrow"></i>
+        </div>
+        <span>${address}</span>
+      </div>
+      <div class="store-info-phone">
+        <div class="icon">
+          <i class="fas fa-phone-alt"></i>
+        </div>
+        <span><a href="tel:${phone}">${phone}</a></span>
+      </div>
+    </div>
+  `;
   let marker = new google.maps.Marker({
     position: latlng,
     map: map,
-    title: 'no idea'
+    label: `${storeNumber + 1}`
   })
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infoWindow.setContent(html);
+    infoWindow.open(map, marker);
+  });
+  markers.push(marker);
 }
 
-initMap();
+initMap()
